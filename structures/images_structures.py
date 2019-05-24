5a621c39e1488073f2af905ec012f84406d573af
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-05-24 16:46:31 +0300

    add image compressing

--- structures/images_structures.py
+++ structures/images_structures.py
@@ -6,6 +6,11 @@ class ImageWrapper:
     def __init__(self, image_path, image_name):
         self.name = image_name
         self.image_obj = Image.open(image_path)
+        self.compress_image()
+
+    def compress_image(self):
+        if self.name == '15.jpg':
+            self.image_obj = self.image_obj.resize((100, 100), Image.ANTIALIAS)
 
 
 class ImagesContainer:
