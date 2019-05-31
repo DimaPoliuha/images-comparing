3d8d93212be5abb0efc71596f6fb3ed1dfc2b34d
DmitryPoliuha <dmitry.polyuha@gmail.com>
2019-05-31 19:48:46 +0300

    add documentation

--- structures/images_structures.py
+++ structures/images_structures.py
@@ -1,92 +1,129 @@
 import numpy as np
-from PIL import Image
-from PIL.ImageFilter import SMOOTH_MORE
+from PIL import Image, ImageFilter
 
 import sift
 
 
 class ImageWrapper:
+    """
+    Wrapper for the image to keep image obj and image name
+    """
 
     def __init__(self, image_path, image_name):
         self.name = image_name
+        self.path = image_path
+        self.image = self.process_image()
+        self.key_points = []
 
-        image = Image.open(image_path)
-        grayscaled_image = image.convert('L')
-        self.image = grayscaled_image
+    def process_image(self):
+        """
+        Reads image and grayscale it
+        :return: grayscale image
+        """
+        image = Image.open(self.path)
+        grayscale_image = image.convert('L')
+        return grayscale_image
 
-        # cropped_img = grayscaled_image.resize((512, 512), Image.BILINEAR)
-        # self.image = np.array(cropped_img)
+    def set_key_points(self, key_points):
+        """
+        Setter for fey points array
+        :param key_points:
+        :return:
+        """
+        self.key_points = key_points
 
 
 class ImagesContainer:
+    """
+    Container to store images and provide comparing logic
+    """
 
     def __init__(self, images_paths, images_names):
-        # self.images = [ImageWrapper(images_paths[15], images_names[15]), ImageWrapper(images_paths[14], images_names[14])]
         self.images = [ImageWrapper(path, name) for (path, name) in zip(images_paths, images_names)]
-        self.sift = [sift.SIFT(image) for image in self.images]
-        self.compare_imgs()
 
-    def compare_imgs(self):
-        out_str = ''
-        # (octave_i, scale_i, m, n, sigma, x, y, omega)
-        imgs_info = [(self.sift[i].extremes, self.sift[i].image.name) for i in range(len(self.sift))]
+    def compute_images_key_points(self):
+        """
+        Computes key points for every image using SIFT key point extraction
+        :return:
+        """
+        sift_detector = sift.SIFT()
+        for image in self.images:
+            image.set_key_points(sift_detector.get_key_points(image.image))
 
+    def compare_images(self):
+        """
+        Provides images comparison logic
+        :return:
+        """
         c_match = 0.6
+        mse_threshold = 2500
 
-        # for k in range(len(self.images)):
-        #     for t in range(k + 1, len(self.images)):
-        #         m = self.mse(self.images[k].image, self.images[t].image)
-                # from skimage import measure
-                # s = measure.compare_ssim(self.images[k].image, self.images[t].image)
-                # if m < 4000 and s > 0.45:
-                #     print(f'{self.images[k].name}\t\t\t\t{self.images[t].name}\t\t\t\tmse: {m}\t\t\tssim: {s}')
+        for k in range(len(self.images)):
+            points_left = self.images[k].key_points
+            for t in range(k + 1, len(self.images)):
+                images_mse = self.calculate_mse(self.images[k].image, self.images[t].image)
 
-        for k in range(len(imgs_info)):
-            for t in range(k + 1, len(imgs_info)):
-                m = self.mse(self.images[k].image, self.images[t].image)
-                # if m < 4000:
-                #     print(f'{imgs_info[k][1]}\t\t{imgs_info[t][1]}\tmse: {m}')
+                # the lower mean squared error - more similar images
+                # check if MSE lower than MSE threshold value and continue comparison
+                if images_mse < mse_threshold:
+                    points_right = self.images[t].key_points
 
-                points_left = [(point[5], point[6]) for point in imgs_info[k][0]]
-                points_right = [(point[5], point[6]) for point in imgs_info[t][0]]
-                # points_left_scalar = [(point[0] ** 2 + point[1] ** 2) ** 0.5 for point in points_left]
+                    matches = 0
+                    # similarity threshold
+                    # 25% of points from first image
+                    matches_threshold = len(points_left) / 4
+                    for i in range(len(points_left)):
+                        distances = []
+                        for j in range(len(points_right)):
+                            # calculate distances between key point from first image and key points from second images
+                            distances.append(self.calculate_distance_between_points(points_left[i], points_right[j]))
 
-                matches = 0
-                for i in range(len(points_left)):
-                    distances = []
-                    for j in range(len(points_right)):
-                        distances.append(self.calculate_distance(points_left[i], points_right[j]))
-                    distances.sort()
-                    first_neighbour = distances[0]
-                    second_neighbour = distances[1]
-                    if first_neighbour < c_match * second_neighbour:
-                        matches += 1
+                        # find first and second neighbours to this key point from first image
+                        distances.sort()
+                        first_neighbour = distances[0]
+                        second_neighbour = distances[1]
 
-                if matches >= len(self.sift[k].extremes) / 4 and m < 2500:
-                    out_str += (f'{imgs_info[k][1]}\t\t{imgs_info[t][1]}\n'
-                                f'{len(self.sift[k].extremes)}\t{round(matches / len(self.sift[k].extremes), 2)}\t{len(self.sift[t].extremes)}\t\tmse: {m}\n\n')
-        with open('results.txt', 'w') as f:
-            f.write(out_str)
+                        # check if this point is similar on two images
+                        if first_neighbour < c_match * second_neighbour:
+                            matches += 1
+                            # if matches more, or same as matching threshold -> this images are similar
+                            if matches >= matches_threshold:
+                                print(f'{self.images[k].name}\t{self.images[t].name}')
+                                break
+                    if matches >= len(points_left) / 4:
+                        break
 
-    def calculate_distance(self, a, b):
+    @staticmethod
+    def calculate_distance_between_points(a, b):
+        """
+        Calculates distance between two points
+        :param a:
+        :param b:
+        :return: distance
+        """
         return ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** 0.5
 
-    def mse(self, imageA, imageB):
-        cropped_img = imageA.resize((256, 256), Image.BILINEAR)
-        filtered_image = cropped_img.filter(SMOOTH_MORE)
-        imageA = np.array(filtered_image)
+    @staticmethod
+    def calculate_mse(left_image, right_image):
+        """
+        Calculates mean squared error between two images
+        :param left_image:
+        :param right_image:
+        :return: mse value
+        """
 
-        cropped_img = imageB.resize((256, 256), Image.BILINEAR)
-        filtered_image = cropped_img.filter(SMOOTH_MORE)
-        # filtered_image = cropped_img.filter(ImageFilter.GaussianBlur(radius=1.6))
-        imageB = np.array(filtered_image)
+        # crop and smooth the first image
+        cropped_img = left_image.resize((256, 256), Image.BILINEAR)
+        filtered_image = cropped_img.filter(ImageFilter.SMOOTH_MORE)
+        left_image = np.array(filtered_image)
 
-        # the 'Mean Squared Error' between the two images is the
-        # sum of the squared difference between the two images;
-        # NOTE: the two images must have the same dimension
-        err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-        err /= float(imageA.shape[0] * imageA.shape[1])
+        # crop and smooth the second image
+        cropped_img = right_image.resize((256, 256), Image.BILINEAR)
+        filtered_image = cropped_img.filter(ImageFilter.SMOOTH_MORE)
+        right_image = np.array(filtered_image)
 
-        # return the MSE, the lower the error, the more "similar"
-        # the two images are
+        # sum of the squared difference between the two images
+        err = np.sum((left_image.astype("float") - right_image.astype("float")) ** 2)
+        err /= float(left_image.shape[0] * left_image.shape[1])
+
         return err
